98fc74937dec3b1c0a9dd3ac0ad59fa113364f9a
drinkfish13 <53922995+drinkfish13@users.noreply.github.com>
2019-10-11 13:53:51 -0500

    comment1

---
+++ lesson1.py
@@ -0,0 +1,6 @@
+long_phrase = 'Насколько проще было бы писать программы если бы не заказчики'
+short_phrase = '640Кб должно хватать для любых задач. Билл Гейтс (по легенде)'
+if len(long_phrase) <= len(short_phrase):
+    print('False')
+else:
+    print('True')
